29256d222e68df3cdd135b2e4b773cfb16b2884b
LebronBrianSinza <lebronbrian23@gmail.com>
2026-01-21 19:20:21 -0500

    Fixed bug

--- app/Models/image.php
+++ app/Models/image.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 
-class image extends Model
+class Image extends Model
 {
     //
 
